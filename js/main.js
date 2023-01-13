b6aa2d57d9afd8bcf837b4d891f2fd36e5692f47
Ivan Monichev <ivan@monichev.ru>
2023-01-14 02:37:20 +0300

    fix: modification code

--- js/main.js
+++ js/main.js
@@ -2,11 +2,20 @@
 
 const randomInteger = (min, max) => {
   if (min < 0 || max < 0) {
-    return 'Диапазон должен быть положительным'
+    return -1;
   }
+
+  if (max < min) {
+    [min, max] = [max, min];
+  }
+
   const random = min + Math.random() * (max + 1 - min);
   return Math.floor(random);
 }
 
+randomInteger(0, 10);
+
 const checkLength = (string, maxLength) => string.length <= maxLength;
+
+checkLength('Same message', 20);
 
