62c700bc69b1e4f0fc38d7f72d3e5ef72f41f843
Ivan Monichev <ivan@monichev.ru>
2023-02-11 01:44:24 +0300

    style: modified the structure

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 import './modules/render-pictures/render-pictures.js';
-import './modules/handle-form/upload-file.js'
-import './modules/handle-form/form-validation.js';
+import './modules/handing-form/upload-file.js'
+import './modules/handing-form/form-validation.js';
